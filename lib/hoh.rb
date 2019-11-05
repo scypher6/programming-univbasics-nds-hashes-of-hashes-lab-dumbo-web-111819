0e6ae574fbe60dd2abbd5c9bf12e74cf0f056334
Joseph Pyram <joseph.pyram@gmail.com>
2019-11-05 00:06:13 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -68,6 +68,7 @@ def naming_system
   # HoH all the way to Species!
   PORTION_1[:sub_category][:sub_category][:sub_category]= PORTION_2
   PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
+  PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
   #PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
   #PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
   
